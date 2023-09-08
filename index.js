d0dde66e97e97998206496627da774b1643fed62
Stensis <irenenjuguna98@gmail.com>
2023-09-08 13:42:52 +0300

    added time in seconds

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = new Date().toLocaleDateString('en-US', { weekday: 'long' });
-document.querySelector('[data-testid="currentUTCTime"]').textContent = new Date().toUTCString().split(' ')[4];
+document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+  new Date().toLocaleDateString("en-US", { weekday: "long" });
+function updateTime() {
+  const date = new Date();
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+  document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  ).textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateTime, 1000);
